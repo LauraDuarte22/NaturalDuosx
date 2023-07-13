fb18c34f74fef98351543d8e0bfc66e534d2aeb2
ccgrupo <laura.duarte@ccgltda.com>
2023-07-12 20:59:15 -0500

    body images

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-// import BannerCarousel from "./components/Banner";
-// import ImageContainer from "./components/ImageContainer";
+
 import BodyComponent from "./components/Body";
 import Cards from "./components/Card";
 import "./App.css";
