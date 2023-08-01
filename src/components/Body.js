5b5f38746cdf95cd9d47df542763976d971942af
ccgrupo <laura.duarte@ccgltda.com>
2023-08-01 18:47:57 -0500

    asesor

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,20 @@
 import React, { useState } from "react";
-import { Container } from "react-bootstrap";
+import { Container, Col, Row } from "react-bootstrap";
 import image from "../assets/Body.png";
-import colageno from "../assets/Productos/Colageno-min.png";
-import vitamina_c from "../assets/Productos/Vitamina C-min.png";
-import vitamina_d from "../assets/Productos/Vitaday Sure-min.png";
+import colageno from "../assets/Productos/Colageno.png";
+import vitamina_c from "../assets/Productos/VitaminaC.png";
+import vitamina_d from "../assets/Productos/VitadaySure.png";
+import VitadaySure from "../assets/Productos/VitadaySure.png";
+import Euximil from "../assets/Productos/Euximil.png";
+import Euximil2 from "../assets/Productos/Euximil2.png";
+import AjoUva from "../assets/Productos/AjoUva.png";
+import GasPlus from "../assets/Productos/GasPlus.png";
+import FibraSlim from "../assets/Productos/FibraSlim.png";
+import Duoprox from "../assets/Productos/Duoprox.png";
+import Uronex from "../assets/Productos/Uronex.png";
+import Reuduol from "../assets/Productos/Reuduol.png";
+import Vitaday from "../assets/Productos/Vitaday.png";
+import asesor from "../assets/Iconos/asesor.png";
 import Product from "./Product";
 import "./styles/Body.css";
 
@@ -11,113 +22,143 @@ const BodyComponent = () => {
   const [selectedPoint, setSelectedPoint] = useState(null);
 
   const pointsData = [
+    //Cerebro
     {
       id: 1,
-      link: [1, 4],
-      x: 675,
-      y: 185,
-      imgName: [colageno],
-    },
-    {
-      id: 2,
-      link: [2, 3],
-      x: 695,
-      y: 185,
-      imgName: [vitamina_c, vitamina_d],
-    },
-    {
-      id: 3,
-      link: [2, 3],
+      link: [1, 3],
       x: 675,
       y: 205,
-      imgName: [vitamina_c, vitamina_d],
+      imgName: [vitamina_c, VitadaySure],
     },
+    //Al lado del cerebro
     {
-      id: 4,
-      link: [1, 4],
-      x: 640,
-      y: 245,
+      id: 2,
+      link: [2, 5],
+      x: 690,
+      y: 200,
       imgName: [colageno],
     },
+    //Nariz
+    {
+      id: 3,
+      link: [1, 3],
+      x: 675,
+      y: 225,
+      imgName: [vitamina_c, VitadaySure],
+    },
+    //Garganta
+    {
+      id: 4,
+      link: [4],
+      x: 675,
+      y: 255,
+      imgName: [vitamina_c, Euximil, Euximil2],
+    },
+    //Hombro
     {
       id: 5,
-      link: [5],
-      x: 680,
-      y: 280,
-      imgName: ["capsula"],
+      link: [5, 2],
+      x: 630,
+      y: 260,
+      imgName: [colageno],
     },
+    //Corazon
     {
       id: 6,
       link: [6],
       x: 690,
-      y: 315,
-      imgName: ["aloeVera"],
+      y: 290,
+      imgName: [AjoUva],
     },
+    //Pulmón
     {
       id: 7,
-      link: [7, 9, 15],
-      x: 735,
-      y: 325,
-      imgName: [colageno, "gel"],
+      link: [7],
+      x: 690,
+      y: 320,
+      imgName: [GasPlus],
     },
+    //Brazo dereho
     {
       id: 8,
-      link: [8],
-      x: 750,
-      y: 425,
-      imgName: ["fibra"],
+      link: [8, 15, 18],
+      x: 725,
+      y: 335,
+      imgName: [colageno, Reuduol],
     },
+    //Intestino
     {
       id: 9,
       link: [7, 9, 15],
-      x: 675,
-      y: 390,
-      imgName: [colageno, "gel"],
+      x: 690,
+      y: 360,
+      imgName: [FibraSlim],
     },
+    //Vegija 1
     {
       id: 10,
       link: [10],
       x: 675,
-      y: 402,
-      imgName: ["duoprox", "uronex"],
+      y: 390,
+      imgName: [Duoprox, Uronex],
     },
+    //Vegija 2
     {
       id: 11,
       link: [11],
       x: 675,
-      y: 415,
-      imgName: ["duoprox"],
+      y: 405,
+      imgName: [Duoprox],
     },
+    //Vegija 3
     {
       id: 12,
       link: [12],
-      x: 715,
-      y: 425,
-      imgName: ["duoprox"],
+      x: 675,
+      y: 420,
+      imgName: [Duoprox],
     },
+    //Hueso derecha
     {
       id: 13,
       link: [13],
-      x: 645,
-      y: 480,
-      imgName: [colageno, vitamina_d, "gel"],
+      x: 710,
+      y: 430,
+      imgName: [colageno, vitamina_d, Reuduol],
     },
+    //Rodilla
     {
       id: 15,
-      link: [7, 9, 15],
-      x: 635,
-      y: 515,
-      imgName: [colageno, "gel"],
+      link: [8, 15, 18],
+      x: 650,
+      y: 480,
+      imgName: [colageno, Reuduol],
     },
+    //Tibia
     {
       id: 16,
       link: [16, 17],
+      x: 635,
+      y: 510,
+      imgName: [Vitaday, Reuduol],
+    },
+    //Gemlos
+    {
+      id: 17,
+      link: [16, 17],
       x: 655,
-      y: 515,
-      imgName: [colageno, "duo"],
+      y: 520,
+      imgName: [Vitaday, Reuduol],
+    },
+    //mano
+    {
+      id: 18,
+      link: [8, 15, 17],
+      x: 740,
+      y: 420,
+      imgName: [colageno, Reuduol],
     },
   ];
-
   const handlePointClick = (pointId) => {
     setSelectedPoint((prevSelectedPoint) =>
       prevSelectedPoint === pointId ? null : pointId
@@ -126,54 +167,109 @@ const BodyComponent = () => {
 
   const renderImages = (point) => {
     if (selectedPoint === point.id) {
-      return point.imgName.map((imageName, index) => (
-        <div
-          key={index}
-          className={`product-container-${
-            index % 2 === 0 ? "left" : "right"
-          }`}
-        >
+      // Filtrar imágenes para la izquierda y la derecha
+      const leftImages = [];
+      const rightImages = [];
+      point.imgName.forEach((imageName, index) => {
+        const product = (
           <Product key={index} index={index} imageName={imageName} />
+        );
+
+        // Agregar imágenes a la izquierda o derecha según su posición en el array
+        if (index % 2 === 0) {
+          leftImages.push(product);
+        } else {
+          rightImages.push(product);
+        }
+      });
+
+      return (
+        <div>
+          <div className="product-container-left">{leftImages}</div>
+          <div className="product-container-right">{rightImages}</div>
         </div>
-      ));
+      );
     }
     return null;
   };
 
   return (
-    <Container
-      className="d-flex align-items-center justify-content-center"
-      style={{ minHeight: "100vh" }}
-    >
-      <div>
-        <img src={image} alt="Cuerpo humano" width={195} />
-        <div className="points-container">
-          {pointsData.map((point) => (
-            <div
-              key={point.id}
-              className={`point ${selectedPoint === point.id ? "selected" : ""}`}
-              style={{
-                top: point.y,
-                left: point.x,
-              }}
-              onClick={() => handlePointClick(point.id)}
-            >
-              {point.link.map((linkIndex) => (
-                <a
-                  key={linkIndex}
-                  className={selectedPoint === point.id ? "selected-link" : ""}
-                >
-                  {/* Renderiza el contenido del enlace aquí */}
-                </a>
+    <>
+      <Container
+        fluid
+        className="d-flex align-items-center justify-content-center"
+      >
+        <Row>
+          <Col md={4} className="d-flex flex-wrap justify-content-center">
+            <div className="product-container">
+              {pointsData.map((point) => (
+                <div key={point.id}>
+                  {point.link.map((linkIndex) => (
+                    <a
+                      key={linkIndex}
+                      className={
+                        selectedPoint === point.id ? "selected-link" : ""
+                      }
+                    ></a>
+                  ))}
+                  {renderImages(point)}
+                </div>
               ))}
             </div>
-          ))}
-        </div>
-      </div>
-      <div className="image-container">
-        {pointsData.map(renderImages)}
-      </div>
-    </Container>
+          </Col>
+
+          <Col md={4} className="d-flex flex-column align-items-center">
+            <img src={image} alt="Cuerpo humano" width={180} />
+            <div className="points-container">
+              {pointsData.map((point) => (
+                <div
+                  key={point.id}
+                  className={`point ${
+                    selectedPoint === point.id ? "selected" : ""
+                  }`}
+                  style={{
+                    top: point.y,
+                    left: point.x,
+                  }}
+                  onClick={() => handlePointClick(point.id)}
+                >
+                  {point.link.map((linkIndex) => (
+                    <a
+                      key={linkIndex}
+                      className={
+                        selectedPoint === point.id ? "selected-link" : ""
+                      }
+                    ></a>
+                  ))}
+                </div>
+              ))}
+            </div>
+          </Col>
+        </Row>
+      </Container>
+      <div className="nube-container">
+  <Container>
+    <Row>
+      <Col md={9} className="d-flex flex-wrap justify-content-center align-items-center">
+        <p className="text-center">
+          ¿Tienes dudas?{" "}
+          <span className="fw-bold">
+            {" "}
+            ¡Habla con <br />
+            un asesor!
+          </span>
+          Escríbenos ahora <br />y recibe tus productos sin
+          <br /> costo adicional ☺️
+        </p>
+      </Col>
+      <Col md={3} className="d-flex flex-wrap justify-content-center align-items-center pe-5">
+        <img src={asesor} alt="Comunícate con nosotros" width={65} height={65} />
+      </Col>
+    </Row>
+  </Container>
+</div>
+
+    </>
   );
 };
 
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,15 +1,52 @@
-import React from "react";
-import "./styles/Product.css"
+import React, { useState } from "react";
+import { Container } from "react-bootstrap";
+import "./styles/Product.css";
 
 const Product = ({ index, imageName }) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
+
   return (
-    <img
-      className={`selected-image ${index % 2 === 0 ? 'image-container-left' : 'image-container-right'}`}
-      width={150}
-      key={index}
-      src={imageName}
-      alt={`Producto ${imageName}`}
-    />
+    <div
+      className={`selected-image-container ${
+        index % 2 === 0 ? "image-container-left" : "image-container-right"
+      }`}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+    {isHovered &&
+      <div className="background-svg">
+        <svg xmlns="http://www.w3.org/2000/svg" width="204" height="232" viewBox="0 0 204 232" fill="none">
+          <rect width="204" height="231.148" rx="31.0266" fill="url(#paint0_linear_403_145433)"/>
+          <defs>
+            <linearGradient id="paint0_linear_403_145433" x1="102.388" y1="29.8631" x2="102" y2="231.148" gradientUnits="userSpaceOnUse">
+              <stop stop-color="white" stop-opacity="0.46"/>
+              <stop offset="1" stop-color="white" stop-opacity="0.04"/>
+            </linearGradient>
+          </defs>
+        </svg>
+      </div> }
+
+    
+      <img
+        className="selected-image image-fluid "
+       
+        key={index}
+        src={imageName}
+        alt={`Producto ${imageName}`}
+      />
+
+      {/* Texto que se mostrará al hacer hover */}
+      {isHovered && <p className="hover-text">hover</p>}
+    </div>
   );
 };
 
